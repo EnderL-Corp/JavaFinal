41c27823f16bb3b7f10b1d7caf234ba8f54095f6
Srihari S <srihari_subramanian@yahoo.com>
2017-05-04 21:58:36 -0400

    My Best Commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,15 @@
 package main;
+import java.awt.*; 
+import javax.swing.*;
 
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello have fun");
+	public static void main(String[] args) {		//i guess u could say the penis is in vagina
+		JFrame j = new JFrame("Vagina");
+		JPanel p = new JPanel();
+		JLabel l = new JLabel("Penis");
+		p.add(l);
+		j.add(p);
+		j.setSize(300, 400);
+		j.setVisible(true);
 	}
 }
