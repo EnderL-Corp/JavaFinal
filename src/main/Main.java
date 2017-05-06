2fff546f1fec8bf93c33d3fb294882bb3149c5ca
Srihari S <srihari_subramanian@yahoo.com>
2017-05-05 20:25:20 -0400

    just trying to work out this jframe

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,10 +3,10 @@ import java.awt.*;
 import javax.swing.*;
 
 public class Main {
-	public static void main(String[] args) {		//i guess u could say the penis is in vagina
-		JFrame j = new JFrame("Vagina");
+	public static void main(String[] args) {
+		JFrame j = new JFrame("Title");
 		JPanel p = new JPanel();
-		JLabel l = new JLabel("Penis");
+		JLabel l = new JLabel("JFrame test");
 		p.add(l);
 		j.add(p);
 		j.setSize(300, 400);
