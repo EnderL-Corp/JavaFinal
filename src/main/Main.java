b9bfaf3b0a88f7d58afd21476d258faad07647aa
Srihari S <srihari_subramanian@yahoo.com>
2017-05-05 23:27:55 -0400

    Added an action listener to the button. Look at console window

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,25 @@
 package main;
-import java.awt.*; 
-import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Main {
 	public static void main(String[] args) {
 		JFrame j = new JFrame("Title");
 		JPanel p = new JPanel();
 		JLabel l = new JLabel("JFrame test");
-		p.add(l);
+		JButton b = new JButton("Test Action"); 
+		b.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Action Performed.");
+			}
+		});
+		//p.add(l);
+		p.add(b);
 		j.add(p);
 		j.setSize(300, 400);
 		j.setVisible(true);
