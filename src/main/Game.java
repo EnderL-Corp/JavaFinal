9ad446ed524fd5091df045e35cf8f0302fa3c34d
Srihari S <srihari_subramanian@yahoo.com>
2017-05-07 08:47:34 -0400

    Game class fixed

--- src/main/Game.java
+++ src/main/Game.java
@@ -2,11 +2,7 @@ package main;
 
 public class Game extends GameClient{
 	public static void main(String[] args) {
-		try{
-			System.out.println("" + java.net.InetAddress.getLocalHost().getHostAddress());
-		} catch(Exception e) {
-			System.out.println(e);
-		}
-		//connectToServer();
+		Game g = new Game();
+		g.connectToServer();
 	}
 }
