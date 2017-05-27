1e6d872c327eb72f2f485f5b057af47d2b94424f
Srihari S <srihari_subramanian@yahoo.com>
2017-05-26 22:26:42 -0400

    boardbutton added

---
+++ src/graphics/BoardButton.java
@@ -0,0 +1,21 @@
+package graphics;
+
+import javax.accessibility.Accessible;
+import javax.swing.AbstractButton;
+import javax.swing.DefaultButtonModel;
+import javax.swing.Icon;
+
+public class BoardButton extends AbstractButton implements Accessible{
+
+	private static final long serialVersionUID = 1L;
+	
+	public BoardButton(String text) {
+		this(text, null);
+	}
+	
+	public BoardButton(String text, Icon icon) {
+        setModel(new DefaultButtonModel());
+
+        init(text, icon);
+	}
+}
