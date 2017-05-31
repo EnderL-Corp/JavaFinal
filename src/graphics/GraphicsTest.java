0f1038556864468897b762955f43cfedb149e70e
LudicrousLoki <snaticallegari@gmail.com>
2017-05-30 23:29:29 -0400

    Quick Prototype
    not finished, had a slight problem, no idea why it wont work

--- src/graphics/GraphicsTest.java
+++ src/graphics/GraphicsTest.java
@@ -1,79 +1,50 @@
 package graphics;
-import java.util.Scanner;  
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Scanner;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
 
-public class GraphicsTest 
-{
-	public static void main(String[] args)
-	{
-		JFrame frame = new JFrame();
-		Scanner kb = new Scanner(System.in);
-		
-		frame.setSize(1280,720);
-		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		frame.setResizable(false);
-		
-		BoardPanel panel = new BoardPanel();
-		frame.add(panel);
-		frame.setVisible(true);		
-		
-		
-		
-		
-		/*
-		boolean close = false;
-		
-		while(close == false)
-		{
-
-			System.out.println("Enter a command:");  
-			String command = kb.next();
-			
-			switch(command)
-			{
-				case "CngComPos":
-					System.out.println("Enter X cordinate:");  
-					int x = kb.nextInt(); //obviously check preconditions
-					System.out.println("Enter Y cordinate:");  
-					int y = kb.nextInt(); //same for this one
-					panel.changeCommanderPos(x,y);
-					break;
-					
-				case "CngMapSize":
-					System.out.println("Enter map Type"); 
-					String type = kb.next(); //precondition check
-					panel.changeMapSize(type);
-					break;
-					
-				case "GetComX":
-					System.out.println(panel.getCommanderX());
-					break;
-					
-				case "GetComY":
-					System.out.println(panel.getCommanderY());
-					break;
-					
-				case "Help":
-					System.out.println("To change commander position, type \"CngComPos\"");
-					System.out.println("To change map size, type \"CngMapSize\"");
-					System.out.println("To get commander coordinates, type \"GetComX\" and \"GetComY\" for their respective values");
-					System.out.println("To end this loop, type \"Close\"");
-					break;
-					
-				case "Close":
-					close = true;
-					break;
-					
-				default:
-					System.out.println("Invalid command, type \"Help\" for list of valid commands");
-					break;
-			}
-			
-			frame.setVisible(true);
-		}
-		*/
+public class GraphicsTest extends JFrame implements ActionListener {
+	public static void main(String[] args) {
+		new GraphicsTest().setVisible(true);			
 	}
+		private GraphicsTest() {
+			super("Testing");
+				
+			setSize(1280,720);
+			setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+			setResizable(false);
+			
+			setLayout(new FlowLayout());
+			JButton host = new JButton("Host Game");
+			host.setActionCommand("host");
+			host.addActionListener(this);
+			JButton join = new JButton("Join Game");
+			join.setActionCommand("join");
+			join.addActionListener(this);
+			JButton quit = new JButton("Quit Game");
+			quit.setActionCommand("quit");
+			quit.addActionListener(this);
+			add(host);
+			add(join);
+			add(quit);
+		
+		}
+		public void actionPreformed(ActionEvent e){
+		 String a = e.getActionCommand();
+		 if(a.equals("host"))
+			System.out.println("game hosting");
+		 
+		 else if(a.equals("join"))
+			 System.out.println("searching for host");
+		 
+		 else
+			 System.out.println("am quiting");
+		}
+		
 }
