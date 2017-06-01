7889c5fe7b35982655eedc768100bd122fd1295d
LudicrousLoki <snaticallegari@gmail.com>
2017-05-31 22:08:59 -0400

    Working text gui
    got a base for the main menu

--- src/graphics/GraphicsTest.java
+++ src/graphics/GraphicsTest.java
@@ -6,10 +6,17 @@ import java.util.Scanner;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
 
 public class GraphicsTest extends JFrame implements ActionListener {
+
+	private static final long serialVersionUID = 1L;
+	JTextField  ip;
+	JButton host;
+	JButton join;
+	JButton quit;
 	public static void main(String[] args) {
 		new GraphicsTest().setVisible(true);			
 	}
@@ -19,32 +26,52 @@ public class GraphicsTest extends JFrame implements ActionListener {
 			setSize(1280,720);
 			setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 			setResizable(false);
-			
 			setLayout(new FlowLayout());
-			JButton host = new JButton("Host Game");
+			
+			ip = new JTextField("Enter IP", 30);
+			ip.setActionCommand("ip");
+			ip.addActionListener(this);
+			host = new JButton("Host Game");
 			host.setActionCommand("host");
 			host.addActionListener(this);
-			JButton join = new JButton("Join Game");
+			join = new JButton("Join Game");
 			join.setActionCommand("join");
 			join.addActionListener(this);
-			JButton quit = new JButton("Quit Game");
+			join.setEnabled(false);
+			quit = new JButton("Quit Game");
 			quit.setActionCommand("quit");
 			quit.addActionListener(this);
+
+			
 			add(host);
 			add(join);
+			add(ip);
 			add(quit);
 		
 		}
-		public void actionPreformed(ActionEvent e){
-		 String a = e.getActionCommand();
-		 if(a.equals("host"))
-			System.out.println("game hosting");
-		 
-		 else if(a.equals("join"))
-			 System.out.println("searching for host");
-		 
-		 else
-			 System.out.println("am quiting");
-		}
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			 String a = e.getActionCommand();
+			 if(a.equals("host"))
+				System.out.println("game hosting");
+			 //launch server
+			 //launch game connecting to server via PC ip
+			 
+			 else if(a.equals("ip")){
+				 System.out.println("typing ip");
+				 join.setEnabled(true); 
+				 System.out.println(ip.getText());
+			 }
+			 else if(a.equals("join")){
+				 System.out.println("searching for host");
+				 //launch game searching for specified ip
+			 }
+			 else if(a.equals("quit")){
+				 System.out.println("am quiting");
+			     setVisible(false);
+			     dispose();
+		     }
+			 }
 		
 }
