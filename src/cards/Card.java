ec2b187b7435c06d25745034985c1c051939ee68
Srihari S <srihari_subramanian@yahoo.com>
2017-06-05 15:13:31 -0400

    some changes

--- src/cards/Card.java
+++ src/cards/Card.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 public abstract class Card implements Serializable			//DO NOT REMOVE SERIALIZABLE. THIS IS NEEDED FOR SENDING OVER NETWORK
 {
+	private static final long serialVersionUID = 1L;
 	protected String name, description;
 	
 	public Card(String nm, String desc)
