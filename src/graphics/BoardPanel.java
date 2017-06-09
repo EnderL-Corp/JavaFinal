eb81061690d83361150889a101c1344f0271cab9
Srihari S <srihari_subramanian@yahoo.com>
2017-06-09 12:27:16 -0400

    trying to put buttons on a frame

--- src/graphics/BoardPanel.java
+++ src/graphics/BoardPanel.java
@@ -10,6 +10,66 @@ import javax.swing.JPanel;
 public class BoardPanel extends JPanel {
 	private int numTiles = 15;
 	private int commanderPosX, commanderPosY;
+	private Color[][] players = {
+			{ Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK },
+			{ Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK },
+			{ Color.BLACK, Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK, Color.BLACK },
+			{ Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK },
+			{ Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK },
+			{ Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK },
+			{ Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK },
+			{ Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK },
+			{ Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK },
+			{ Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK },
+			{ Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK },
+			{ Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK },
+			{ Color.BLACK, Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK, Color.BLACK },
+			{ Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK },
+			{ Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK } };
+
+	/*
+	 * public void paintComponent(Graphics g) { int height = 650; int width =
+	 * 650; int tileHeight = height / numTiles; int tileWidth = width /
+	 * numTiles;
+	 * 
+	 * boolean fill = false;
+	 * 
+	 * for (int i = 0; i < numTiles; i++) { for (int j = 0; j < numTiles; j++) {
+	 * // Graphics2D g = new Graphics2D() // Graphics2D g2 = (Graphics2D) g;
+	 * g.setColor(Color.black);
+	 * 
+	 * if ((i == 0 || i == numTiles - 1 || j == 0 || j == numTiles - 1) //
+	 * border || ((i == 1 || i == numTiles - 2) && (j == 1 || j == 2 || j == 3
+	 * || j == 4 || j == numTiles - 2 || j == numTiles - 3 || j == numTiles - 4
+	 * || j == numTiles - 5)) // next // col // in || ((i == 2 || i == numTiles
+	 * - 3) && (j == 1 || j == 2 || j == numTiles - 2 || j == numTiles - 3)) //
+	 * next // col // in || ((i == 3 || i == 4 || i == numTiles - 4 || i ==
+	 * numTiles - 5) && (j == 1 || j == numTiles - 2)) // next col in || ((i ==
+	 * 5 || i == numTiles - 6) && (j == 6 || j == 7 || j == numTiles - 7)) //
+	 * middle // out || ((i == 6 || i == 7 || i == numTiles - 7) && (j == 5 || j
+	 * == 6 || j == 7 || j == numTiles - 7 || j == numTiles - 6))) // middle {
+	 * g.fillRect(i * tileWidth, j * tileHeight, tileWidth, tileHeight); }
+	 * 
+	 * if (((i == 0 || i == numTiles - 1) && (j == 0 || j == 1 || j == numTiles
+	 * - 2 || j == numTiles - 1)) || ((i == 1 || i == numTiles - 2) && (j == 0
+	 * || j == numTiles - 1))) { g.fillRect(i * tileWidth, j * tileHeight,
+	 * tileWidth, tileHeight); }
+	 * 
+	 * 
+	 * if (fill) { g.fillRect(i*tileWidth,j*tileHeight,tileWidth,tileHeight); }
+	 * 
+	 * else { g.drawRect(i * tileWidth, j * tileHeight, tileWidth, tileHeight);
+	 * }
+	 * 
+	 * if (j == commanderPosX && i == commanderPosY) { g.setColor(Color.red);
+	 * g.fillRect(i * tileWidth, j * tileHeight, tileWidth, tileHeight); }
+	 * 
+	 * fill = !fill; }
+	 * 
+	 * if (numTiles%2 == 0) { fill = !fill;
+	 * 
+	 * } } }
+	 */
 
 	public void paintComponent(Graphics g) {
 		int height = 650;
@@ -21,53 +81,29 @@ public class BoardPanel extends JPanel {
 
 		for (int i = 0; i < numTiles; i++) {
 			for (int j = 0; j < numTiles; j++) {
-				// Graphics2D g = new Graphics2D()
-				// Graphics2D g2 = (Graphics2D) g;
-				g.setColor(Color.black);
 
-				if ((i == 0 || i == numTiles - 1 || j == 0 || j == numTiles - 1) // border
-						|| ((i == 1 || i == numTiles - 2) && (j == 1 || j == 2 || j == 3 || j == 4 || j == numTiles - 2
-								|| j == numTiles - 3 || j == numTiles - 4 || j == numTiles - 5)) // next
-																									// col
-																									// in
-						|| ((i == 2 || i == numTiles - 3)
-								&& (j == 1 || j == 2 || j == numTiles - 2 || j == numTiles - 3)) // next
-																									// col
-																									// in
-						|| ((i == 3 || i == 4 || i == numTiles - 4 || i == numTiles - 5)
-								&& (j == 1 || j == numTiles - 2)) // next col in
-						|| ((i == 5 || i == numTiles - 6) && (j == 6 || j == 7 || j == numTiles - 7)) // middle
-																										// out
-						|| ((i == 6 || i == 7 || i == numTiles - 7)
-								&& (j == 5 || j == 6 || j == 7 || j == numTiles - 7 || j == numTiles - 6))) // middle
-				{
-					g.fillRect(i * tileWidth, j * tileHeight, tileWidth, tileHeight);
-				}
-
-				if (((i == 0 || i == numTiles - 1) && (j == 0 || j == 1 || j == numTiles - 2 || j == numTiles - 1))
-						|| ((i == 1 || i == numTiles - 2) && (j == 0 || j == numTiles - 1))) {
-					g.fillRect(i * tileWidth, j * tileHeight, tileWidth, tileHeight);
-				}
-
-				/*
-				 * if (fill) {
-				 * g.fillRect(i*tileWidth,j*tileHeight,tileWidth,tileHeight); }
-				 */
-				else {
-					g.drawRect(i * tileWidth, j * tileHeight, tileWidth, tileHeight);
-				}
+				BoardButton bb = new BoardButton();
+				bb.setBackground(players[i][j]);
+				bb.setBounds(i * tileWidth, j * tileHeight, tileWidth, tileHeight);
+				this.add(bb);
 
 				if (j == commanderPosX && i == commanderPosY) {
-					g.setColor(Color.red);
-					g.fillRect(i * tileWidth, j * tileHeight, tileWidth, tileHeight);
+					bb.setBackground(Color.RED);
+					bb.setBounds(i * tileWidth, j * tileHeight, tileWidth, tileHeight);
+					this.add(bb);
 				}
-				/*
-				 * fill = !fill; }
-				 * 
-				 * if (numTiles%2 == 0) { fill = !fill;
-				 */
 			}
 		}
+		/*BoardButton bb = new BoardButton();
+		bb.setBounds(1 * tileWidth, 1 * tileHeight, tileWidth, tileHeight);
+		bb.setBackground(Color.RED);
+		this.add(bb);
+		System.out.println("yuh");*/
+	}
+	
+	 public void redo(Graphics g) {
+		
+
 	}
 
 	public void changeCommanderPos(int x, int y) {
--- src/main/SpriteTester.java
+++ src/main/SpriteTester.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -13,15 +14,17 @@ import javax.swing.JPanel;
 
 import com.sun.prism.Graphics;
 
+import graphics.BoardButton;
+
 public class SpriteTester {
 	public static void main(String[] args) {
 		JFrame f = new JFrame();
 		JPanel p = new JPanel();/*
 		Graphics g = (Graphics) p.getGraphics();*/
-		ImageIcon myImage = new ImageIcon("Sprites/SpriteTest");
-		JButton b = new JButton();
-		Graphics g = (Graphics) b.getGraphics();
-		b.setIcon(myImage);
+		ImageIcon myImage = new ImageIcon("Sprites/TestSprite.png");
+		BoardButton b = new BoardButton("Hello", myImage);
+		b.setBackground(Color.BLACK);
+		//b.setIcon(myImage);
 		p.add(b);
 		f.add(p);
 		f.setSize(500, 500);
