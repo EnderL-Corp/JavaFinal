ac22a326ce6ce92549b056128224544e91850117
Srihari S <srihari_subramanian@yahoo.com>
2017-06-09 13:10:36 -0400

    aiehf j

--- src/graphics/BoardPanel.java
+++ src/graphics/BoardPanel.java
@@ -10,22 +10,74 @@ import javax.swing.JPanel;
 public class BoardPanel extends JPanel {
 	private int numTiles = 15;
 	private int commanderPosX, commanderPosY;
-	private Color[][] players = {
-			{ Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK },
-			{ Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK },
-			{ Color.BLACK, Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK, Color.BLACK },
-			{ Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK },
-			{ Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK },
-			{ Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK },
-			{ Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK },
-			{ Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK },
-			{ Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK },
-			{ Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK },
-			{ Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK },
-			{ Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK },
-			{ Color.BLACK, Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK, Color.BLACK },
-			{ Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK },
-			{ Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK } };
+	/*
+	 * private Color[][] players = { { Color.BLACK, Color.BLACK, Color.BLACK,
+	 * Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK,
+	 * Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK,
+	 * Color.BLACK, Color.BLACK }, { Color.BLACK, Color.BLACK, Color.BLACK,
+	 * Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE,
+	 * Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK, Color.BLACK,
+	 * Color.BLACK, Color.BLACK }, { Color.BLACK, Color.BLACK, Color.BLACK,
+	 * Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE,
+	 * Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK,
+	 * Color.BLACK, Color.BLACK }, { Color.BLACK, Color.BLACK, Color.WHITE,
+	 * Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE,
+	 * Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE,
+	 * Color.BLACK, Color.BLACK }, { Color.BLACK, Color.BLACK, Color.WHITE,
+	 * Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE,
+	 * Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE,
+	 * Color.BLACK, Color.BLACK }, { Color.BLACK, Color.WHITE, Color.WHITE,
+	 * Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK,
+	 * Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE,
+	 * Color.WHITE, Color.BLACK }, { Color.BLACK, Color.WHITE, Color.WHITE,
+	 * Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK, Color.BLACK,
+	 * Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE,
+	 * Color.WHITE, Color.BLACK }, { Color.BLACK, Color.WHITE, Color.WHITE,
+	 * Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK, Color.BLACK,
+	 * Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE,
+	 * Color.WHITE, Color.BLACK }, { Color.BLACK, Color.WHITE, Color.WHITE,
+	 * Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK, Color.BLACK,
+	 * Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE,
+	 * Color.WHITE, Color.BLACK }, { Color.BLACK, Color.WHITE, Color.WHITE,
+	 * Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK,
+	 * Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE,
+	 * Color.WHITE, Color.BLACK }, { Color.BLACK, Color.BLACK, Color.WHITE,
+	 * Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE,
+	 * Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE,
+	 * Color.BLACK, Color.BLACK }, { Color.BLACK, Color.BLACK, Color.WHITE,
+	 * Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE,
+	 * Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE,
+	 * Color.BLACK, Color.BLACK }, { Color.BLACK, Color.BLACK, Color.BLACK,
+	 * Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE,
+	 * Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK,
+	 * Color.BLACK, Color.BLACK }, { Color.BLACK, Color.BLACK, Color.BLACK,
+	 * Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE,
+	 * Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK, Color.BLACK,
+	 * Color.BLACK, Color.BLACK }, { Color.BLACK, Color.BLACK, Color.BLACK,
+	 * Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK,
+	 * Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK,
+	 * Color.BLACK, Color.BLACK } };
+	 */
+	
+	public BoardPanel() {
+		start();
+	}
+	
+	public void start() {
+		int height = 650;
+		int width = 650;
+		int tileHeight = height / numTiles;
+		int tileWidth = width / numTiles;
+		
+		for (int i = 0; i < numTiles; i++) {
+			for (int j = 0; j < numTiles; j++) {
+				BoardButton bb = new BoardButton();
+				bb.setBackground(Color.WHITE);
+				bb.setBounds(i * tileWidth, j * tileHeight, tileWidth, tileHeight);
+				this.add(bb);
+			}
+		}
+	}
 
 	/*
 	 * public void paintComponent(Graphics g) { int height = 650; int width =
@@ -77,32 +129,42 @@ public class BoardPanel extends JPanel {
 		int tileHeight = height / numTiles;
 		int tileWidth = width / numTiles;
 
-		boolean fill = false;
-
 		for (int i = 0; i < numTiles; i++) {
 			for (int j = 0; j < numTiles; j++) {
+				// Graphics2D g = new Graphics2D() // Graphics2D g2 =
+				// (Graphics2D) g;
 
-				BoardButton bb = new BoardButton();
-				bb.setBackground(players[i][j]);
-				bb.setBounds(i * tileWidth, j * tileHeight, tileWidth, tileHeight);
-				this.add(bb);
+				//BLACK STUFF
+				if ((i == 0 || i == numTiles - 1 || j == 0 || j == numTiles - 1) 
+						|| ((i == 1 || i == numTiles - 2) && (j == 1 || j == 2 || j == 3 || j == 4 || j == numTiles - 2
+								|| j == numTiles - 3 || j == numTiles - 4 || j == numTiles - 5)) 
+						|| ((i == 2 || i == numTiles - 3)
+								&& (j == 1 || j == 2 || j == numTiles - 2 || j == numTiles - 3)) 
+						|| ((i == 3 || i == 4 || i == numTiles - 4 || i == numTiles - 5)
+								&& (j == 1 || j == numTiles - 2))
+						|| ((i == 5 || i == numTiles - 6) && (j == 6 || j == 7 || j == numTiles - 7))
+						|| ((i == 6 || i == 7 || i == numTiles - 7)
+								&& (j == 5 || j == 6 || j == 7 || j == numTiles - 7 || j == numTiles - 6))) {
+					BoardButton bb = new BoardButton();
+					bb.setBackground(Color.BLACK);
+					bb.setBounds(i * tileWidth, j * tileHeight, tileWidth, tileHeight);
+					this.add(bb);
+				}
+				else {
+					BoardButton bb = new BoardButton();
+					bb.setBackground(Color.WHITE);
+					bb.setBounds(i * tileWidth, j * tileHeight, tileWidth, tileHeight);
+					this.add(bb);
+				}
 
 				if (j == commanderPosX && i == commanderPosY) {
+					BoardButton bb = new BoardButton();
 					bb.setBackground(Color.RED);
 					bb.setBounds(i * tileWidth, j * tileHeight, tileWidth, tileHeight);
 					this.add(bb);
 				}
 			}
 		}
-		/*BoardButton bb = new BoardButton();
-		bb.setBounds(1 * tileWidth, 1 * tileHeight, tileWidth, tileHeight);
-		bb.setBackground(Color.RED);
-		this.add(bb);
-		System.out.println("yuh");*/
-	}
-	
-	 public void redo(Graphics g) {
-		
 
 	}
 
