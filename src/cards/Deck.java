d11cc4b13412aa7aaa04a1355b9db92246891b48
Srihari S <srihari_subramanian@yahoo.com>
2017-06-15 19:36:05 -0400

    techenum now in deck

--- src/cards/Deck.java
+++ src/cards/Deck.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 import cards.Gear.GearEnum;
+import cards.Technique.TechEnum;
 
 public class Deck implements Serializable{
 	private static final long serialVersionUID = -3276572470774611765L;
@@ -44,8 +45,8 @@ public class Deck implements Serializable{
 			deck.add(new Gear(GearEnum.MIRROR));
 			deck.add(new Gear(GearEnum.SHIELD));
 			deck.add(new Gear(GearEnum.SHIELD));
-			deck.add(new Technique("Cannon", null, 0, 0, null));
-			deck.add(new Technique("Cannon", null, 0, 0, null));
+			deck.add(new Technique(TechEnum.CANNON));
+			deck.add(new Technique(TechEnum.CANNON));
 			
 			cp = 16;
 			ap = 20;
@@ -67,14 +68,14 @@ public class Deck implements Serializable{
 			deck.add(new Android(0,0,0,null));
 			deck.add(new Gear(GearEnum.ELECTROMAGNET));
 			deck.add(new Gear(GearEnum.SHIELD));
-			deck.add(new Technique("Grape Shot", null, 0, 0, null));
-			deck.add(new Technique("Grape Shot", null, 0, 0, null));
-			deck.add(new Technique("Call", null, 0, 0, null));
-			deck.add(new Technique("Chain Shot", null, 0, 0, null));
-			deck.add(new Technique("Drain", null, 0, 0, null));
-			deck.add(new Technique("Cannon", null, 0, 0, null));
-			deck.add(new Technique("Cannon", null, 0, 0, null));
-			deck.add(new Technique("Drain", null, 0, 0, null));
+			deck.add(new Technique(TechEnum.GRAPE_SHOT));
+			deck.add(new Technique(TechEnum.GRAPE_SHOT));
+			deck.add(new Technique(TechEnum.CALL));
+			deck.add(new Technique(TechEnum.CHAIN_SHOT));
+			deck.add(new Technique(TechEnum.DRAIN));
+			deck.add(new Technique(TechEnum.CANNON));
+			deck.add(new Technique(TechEnum.CANNON));
+			deck.add(new Technique(TechEnum.DRAIN));
 			deck.add(new Amplifier("Healing Wave", null, null, 0));
 			deck.add(new Amplifier("Healing Wave", null, null, 0));
 			deck.add(new Amplifier("Short Circuit", null, null, 0));
@@ -107,7 +108,7 @@ public class Deck implements Serializable{
 			deck.add(new Gear(GearEnum.LAUNCHER));
 			deck.add(new Gear(GearEnum.ELECTROMAGNET));
 			deck.add(new Gear(GearEnum.MIRROR));
-			deck.add(new Technique("Call", null, 0, 0, null));
+			deck.add(new Technique(TechEnum.CALL));
 			deck.add(new Amplifier("Dronesday Device", null, null, 0));
 			deck.add(new Amplifier("Dronesday Device", null, null, 0));
 			deck.add(new Amplifier("Overdraw", null, null, 0));
