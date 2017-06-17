28b00c267c23bfafde6321d275a245eae000e5b1
Srihari S <srihari_subramanian@yahoo.com>
2017-06-16 21:25:50 -0400

    javadoc and authorship added for GameServerInterface

--- src/rmi/GameServerInterface.java
+++ src/rmi/GameServerInterface.java
@@ -8,12 +8,31 @@ import java.util.ArrayList;
 import cards.Card;
 import cards.Entity;
 
+/**
+ * Interface so that the client can "interface" with the running server.
+ * @author Srihari Subramanian
+ *
+ */
 public interface GameServerInterface extends Remote, Serializable{
+	
 	String getRecentClientName() throws RemoteException;
+	
+	/**
+	 * method to add this GameClient to the list of the server's known clients
+	 * @param gc the GameClient to be connected
+	 * @throws RemoteException
+	 */
 	void connect(GameClient gc) throws RemoteException;
+	
 	ArrayList<GameClient> getGameClients() throws RemoteException;
+	
 	int getConnections() throws RemoteException;
 	
+	/**
+	 * method to get the current board
+	 * @return the current version of the board of entities
+	 * @throws RemoteException
+	 */
 	Entity[][] getBoard() throws RemoteException;
 	
 	/**
@@ -25,8 +44,12 @@ public interface GameServerInterface extends Remote, Serializable{
 	 * @throws RemoteException
 	 */
 	boolean updateBoard(GameClient gc, Entity[][] updated) throws RemoteException;
+	
 	void endMyTurn() throws RemoteException;
+	
 	int getTurnTag() throws RemoteException;
+	
 	GameClient getWinner() throws RemoteException;
+	
 	void gameOver(GameClient gc) throws RemoteException;
 }
