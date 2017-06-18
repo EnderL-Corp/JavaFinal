3f23268f178dba537916a28cc0e75636ad59b9f6
EnderL2000 <Bluestackthingie@gmail.com>
2017-06-17 22:59:41 -0400

    Card JavaDoc

--- src/cards/Card.java
+++ src/cards/Card.java
@@ -6,6 +6,11 @@ import java.io.Serializable;
 import main.Audio;
 import main.Game;
 
+/**
+ * 
+ * @author Luke, André
+ *
+ */
 public abstract class Card implements Serializable			//DO NOT REMOVE SERIALIZABLE. THIS IS NEEDED FOR SENDING OVER NETWORK
 {
 	private static final long serialVersionUID = 1L;
@@ -14,6 +19,13 @@ public abstract class Card implements Serializable			//DO NOT REMOVE SERIALIZABL
 	
 	protected Audio fx;
 	
+	/**
+	 * A card is the most basic object in the game. A card is anything from a troop
+	 * to a spell to an amplifier. Every card has a name, a description, and a color
+	 * for the "Team" it is on
+	 * @param nm - The name of the card
+	 * @param desc - The card's description
+	 */
 	public Card(String nm, String desc)
 	{
 		name = nm;
@@ -25,6 +37,9 @@ public abstract class Card implements Serializable			//DO NOT REMOVE SERIALIZABL
 		}
 	}
 	
+	/**
+	 * Special call for <code> Troop.java </code>
+	 */
 	public Card()
 	{
 		return;
