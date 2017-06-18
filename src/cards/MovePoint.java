31886173f74622c1bd108579cdcdca6e5594d1f9
EnderL2000 <Bluestackthingie@gmail.com>
2017-06-18 00:09:12 -0400

    MovePoint and Structure JavaDoc

--- src/cards/MovePoint.java
+++ src/cards/MovePoint.java
@@ -3,6 +3,12 @@ package cards;
 public class MovePoint extends Card {
 	private int xPos, yPos;
 	
+	/**
+	 * Creates a move point with a specified location. It is temporary and
+	 * serves as a way to store just location in a Card form
+	 * @param x - x position
+	 * @param y - y position
+	 */
 	public MovePoint(int x, int y)
 	{
 		super("MovePoint", "A Temporary MovePoint");
--- src/cards/Structure.java
+++ src/cards/Structure.java
@@ -2,10 +2,8 @@ package cards;
 
 public abstract class Structure extends Card
 {
-
 	public Structure(String nm, String desc) 
 	{
 		super(nm, desc);
 	}
-	
 }
