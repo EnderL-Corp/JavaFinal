7dfa2b7c44426371a144d81b761ddd3d1e34649d
LudicrousLoki <snaticallegari@gmail.com>
2017-06-18 12:35:29 -0400

    Stats panel now only has text panes

--- src/graphics/StatsPanel.java
+++ src/graphics/StatsPanel.java
@@ -14,26 +14,18 @@ public class StatsPanel extends JPanel {
 		// add a phat layout
 		stats = new JTextPane();
 		stats.setEditable(false);
-
 		stats.setText("Your HP :" + Game.game.getCommander().getHealth() + "\nFoe's HP :"
 				+ "none" + "\nCP :"
 				+ Game.game.getCP() + "\nTP :" + Game.game.getTP() + "\nAP :" + Game.game.getAP());
 
-		JLabel key1 = new JLabel("A : Toggles attack mode");
-		JLabel key2 = new JLabel("M : Toggles move mode");
-		JLabel key3 = new JLabel("P : Toggles play mode for troops");
-		JLabel key4 = new JLabel("G : Toggles play mode for gear");
-		JLabel key5 = new JLabel("T : Toggles play mode for techniques");
-		JLabel key6 = new JLabel("S : Toggles play mode for structures");
+		JTextPane key = new JTextPane();
+		key.setEditable(false);
+		key.setText("A : Toggles attack mode" + "\nM : Toggles move mode" + "\nP : Toggles play mode for troops"
+		+ "\nG : Toggles play mode for gear" + "\nT : Toggles play mode for techniques" + "\nS : Toggles play mode for structures");
+
 
 		add(stats);
-
-		add(key1);
-		add(key2);
-		add(key3);
-		add(key4);
-		add(key5);
-		add(key6);
+		add(key);
 	}
 
 	public void statRefresh() {
